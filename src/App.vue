501c315d96c8dfcde41bfc568d700da77ba60f6d
oliver6565 <390200763@qq.com>
2022-12-23 16:08:09 +0800

    1223

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div style="background-color: #6B92B0;">
     <router-view></router-view>
+    {{ a.split(',').map(v=>{return v+'ii'}) }}
+    <p v-text="b"></p>
+    <form action="/">
+      <button @click.prevent="f1">click</button>
+      <div :style="style1">style test</div>
+      <div :class="class1">class test</div>
+    </form>
   </div>
 </template>
 
-<script>
-import { reactive, toRefs } from 'vue'
+<script setup>
+const count=0,
+a='我是,一个字符串',
+b= 199929991299,
+f1 =()=>{console.log('???')},
+style1={display:'none'},
+class1={display:'none'}
 
-export default {
-  setup () {
-    const state = reactive({
-      count: 0,
-    })
-  
-    return {
-      ...toRefs(state),
-    }
-  }
-}
 </script>
 
 <style lang="less" scoped>
